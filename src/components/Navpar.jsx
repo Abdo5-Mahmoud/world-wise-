24d821c731747956e7f2f22784c2f67582f91da2
Abdo5-Mahmoud <abdofwzy9@gmali.com>
2025-10-18 20:50:48 +0300

    netlifyToml&logo

--- src/components/Navpar.jsx
+++ src/components/Navpar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import { faBars, faBarsStaggered } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
-
+import NavLogo from "../../assets/logo.png";
 export default function Navbar() {
   const [clicked, setClicked] = useState(false);
   function handleClick() {
@@ -15,7 +15,7 @@ export default function Navbar() {
       <div className="navbarCont">
         <div className="logo">
           <NavLink to={"/"}>
-            <img src="../assets/logo.png" alt="logo" />
+            <img src={NavLogo} alt="logo" />
           </NavLink>
         </div>
         <ul id="navbar">
